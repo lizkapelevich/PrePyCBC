a1ef0a00f5f267a3b9b974b807da4a5459e3fa5a
lizkapelevich <elizakape@gmail.com>
2022-06-19 11:59:27 -0400

    WIP integral function

--- signals_and_noise_introduction.py
+++ signals_and_noise_introduction.py
@@ -6,9 +6,9 @@ def get_noise(t_0, t_end, del_T):
     
     INPUT:
     ------
-      t_0 : start time of a data
-    t_end : end time of a data
-      del_T : interval of time between each value
+    t_0 : start time of noise
+    t_end : end time of noise
+    del_T : interval of time between each value
     
     RETURNS:
     --------
@@ -16,50 +16,49 @@ def get_noise(t_0, t_end, del_T):
     
     """
 
-    duration_data = t_end - t_0               
-    x = duration_data / del_T
-    del_T_prime = duration_data / math.ceil(x)
+    duration_data = t_end - t_0
+    number_of_values = duration_data / del_T                        # calculating number of time stamps
+    del_T_prime = duration_data / math.ceil(number_of_values)       # new interval of the noise data to allow  
+                                                                    # array to reach end point
     
-    time_series = np.arange(t_0, t_end + del_T_prime, del_T_prime)
-    noise = np.random.random(len(time_series))
-
+    time_series = np.arange(t_0, t_end + del_T_prime, del_T_prime)  # array from beginning to end of noise time stamp
+                                                                    # with increments of the standard deviation
+    noise = -1 + 2 * np.random.random(len(time_series))             # random values with same length as time_series
+    
     return (time_series, noise, del_T_prime)
 
 
 def get_signal(A, t_0, t_end, del_T, f, sigma):
-
+    
     """
     This function will take as input the amplitude, boundaries of a time stamp
     from a signal, the frequency, and the standard deviation.
-
+    
     INPUT:
     ------
-        f : frequency
-        A : amplitude
-      t_0 : start time of a signal
+    f : frequency
+    A : amplitude
+    sigma : standard deviation
+    t_0 : start time of a signal
     t_end : end time of a signal
     del_T : interval of time between each value
-    sigma : standard deviation
-
+    
     RETURNS:
     --------
     The time series for a signal.
-
+    
     """
 
-    duration_signal = t_end - t_0
-
-    x = duration_signal / del_T
-
-    del_T_prime = duration_signal / math.ceil(x)
-
+    duration_data = t_end - t_0
+    number_of_values = duration_data / del_T                        # calculating number of time stamps
+    del_T_prime = duration_data / math.ceil(number_of_values)       # new interval of the noise data to allow  
+                                                                    # array to reach end point
     t = np.arange(t_0, t_end + del_T_prime, del_T_prime)
-    t_mean = (t_0 + t_end) / 2
-
-    S = A*np.sin(2*np.pi*f*t)*np.exp((-(t - t_mean)**2)/(2*sigma))
+    
+    t_mean = (t_0 + t_end) / 2                                      # finding center of signal
+    S = A*np.sin(2*np.pi*f*t)*np.exp((-(t - t_mean)**2)/(2*sigma))  # calculating sine gaussian
 
     return (t, S, del_T_prime)
-
 
 
 def final_data(a, t_signal_start, t_signal_end, t_noise_start, t_noise_end,
@@ -68,43 +67,85 @@ def final_data(a, t_signal_start, t_signal_end, t_noise_start, t_noise_end,
     This function will take as input the amplitude, time boundaries
     of the signal and noise, interval between their values, the
     frequency, and standard deviation.
-
+    
     INPUT:
     ------
-             A : amplitude
-             f : frequency
-         sigma : standard deviation
-         del_T : interval of time between each value
-   t_noise_end : end time of noise
-  t_signal_end : end time of signal
- t_noise_start : start time of noise
-t_signal_start : start time of signal
-
+    A : amplitude
+    f : frequency
+    sigma : standard deviation
+    t_noise_end : end time of noise
+    t_signal_end : end time of signal
+    t_noise_start : start time of noise
+    t_signal_start : start time of signal
+    del_T : interval of time between each value
+    
     RETURNS:
     --------
-    The calculation for cross-correlating a signal embedded
-    within random noise with zero-padding if necessary.
-
+    The calculation for embedding a signal within random noise.
+    
     """
-    t1, x, del_T_signal = get_signal(a, t_signal_start, t_signal_end,
+    time_series_signal, signal_values, del_T_signal = get_signal(a, t_signal_start, t_signal_end,
                                      del_T, f, sigma)
-    t2, noise, del_T_noise = get_noise(t_noise_start, t_noise_end, del_T)
-
-    s = interpolate.interp1d(t1, x)
-
-    index1 = t2 > t1[0]
-    index2 = t2 < t1[-1]
-    index = index1*index2
-
-    zeroes = np.zeros_like(noise)
-
-    signal_time_stamps = t2[index]
+    time_series_noise, noise_values, del_T_noise = get_noise(t_noise_start, t_noise_end, del_T)
+    
+    s = interpolate.interp1d(time_series_signal, signal_values)
+    
+    index1 = time_series_noise > time_series_signal[0]   # boolean = False before signal in noise time-series
+    index2 = time_series_noise < time_series_signal[-1]  # boolean = False after signal in noise time_series
+    time_series_intersect = index1*index2                # multiplying arrays into one array of booleans
+    
+    zeroes = np.zeros_like(noise_values)                          # create an array of zeroes same length 
+                                                                  # as noise_values
+    signal_time_stamps = time_series_noise[time_series_intersect] # keeping only boolean = True
     signal = s(signal_time_stamps)
 
-    data_in_signal = noise[index] + signal
-    data_before_signal = noise[index2*(~index)]
-    data_after_signal = noise[index1*(~index)]
+    data_in_signal = noise_values[time_series_intersect] + signal       # embedding signal in noise
+    data_before_signal = noise_values[index2*(~time_series_intersect)]  # prepping to graph noise before signal
+    data_after_signal = noise_values[index1*(~time_series_intersect)]   # prepping to graph noise after signal
 
-    alldata = np.hstack((data_before_signal, data_in_signal, data_after_signal))
+    alldata = np.hstack((data_before_signal, data_in_signal, data_after_signal)) # combining the three arrays
+    
+    return (time_series_noise, alldata)
 
-    return (t2, alldata)
+
+def template(A, f, sigma, t_0, t_duration, data_time_stamps, ad_hoc_grid = 10000):
+
+    """
+    This function will take as input the frequency, standard
+    deviation, initial time, duration time, and interval
+    of time between each value.
+    
+    INPUT:
+    ------
+    A : amplitude
+    f : frequency
+    sigma : standard deviation
+    t_0 : start time of a signal
+    t_duration : duration of time for the signal
+    data_time_stamps : time stamps from the data
+    
+    RETURNS:
+    --------
+    A template that will match the given signal using
+    zero-padding if necessary.
+    
+    """
+    
+    t_end = t_duration + t_0
+    time_stamps = np.linspace(t_0, t_end, ad_hoc_grid)         # template time stamps with random step-size
+    
+    t_mean = np.mean(time_stamps)
+    S = A*np.sin(2*np.pi*f*time_stamps)*np.exp((-(time_stamps - t_mean)**2)/(2*sigma)) # evaluation in ad-hoc grid
+    
+    del_T_data = np.diff(data_time_stamps)[0]
+    
+    s = interpolate.interp1d(time_stamps, S)                   # interpolating signal in time-stamps of data-grid
+    template_time_stamps = np.arange(t_0, t_end, del_T_data)
+    Template = s(template_time_stamps)                         # evaluating sine-gaussian at data-grid points
+    
+    template_series = np.zeros_like(data_time_stamps)          # template time-stamps same length as data time-stamps
+    
+    index_prefix = template_series[data_time_stamps < t_0]     # zero-padding template on the left side
+    index_suffix = template_series[data_time_stamps > t_end]   # zero-padding template on the right side
+    
+    return np.hstack((index_prefix, Template, index_suffix))
